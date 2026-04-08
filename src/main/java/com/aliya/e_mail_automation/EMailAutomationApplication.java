873b470e8fc0498a628a8631d5ccff6d7aeb2704
aliya neda <137129762+aliyaneda0@users.noreply.github.com>
2026-04-09 00:33:31 +0530

    First commit

---
+++ src/main/java/com/aliya/e_mail_automation/EMailAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.aliya.e_mail_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMailAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMailAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliya/e_mail_automation/EMailAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliya.e_mail_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMailAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
